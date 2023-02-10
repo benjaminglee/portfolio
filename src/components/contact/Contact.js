57b309ca5d0db99d45c40e7a978e95fa532c4ebe
Benjamin Lee <benjaminlee0727@gmail.com>
2023-02-10 10:44:43 -0600

    adjust iframe for safari

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -8,10 +8,10 @@ const Contact = () => {
         <h2 id="contactHeader">Contact Me!</h2>
         <div className="contact-main">
           <div className="map-wrapper">
-            {/* <iframe
+            <iframe
               title="google maps showing wicker park in Chicago"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d33997.69821304711!2d-87.70018472935685!3d41.90432088687726!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880fd2c5d9cb201f%3A0x9c95d9bc940da12f!2sWicker%20Park%2C%20Chicago%2C%20IL!5e0!3m2!1sen!2sus!4v1675744685946!5m2!1sen!2sus"
-            ></iframe> */}
+            ></iframe>
           </div>
           <div className="contact-text">
             <p>
